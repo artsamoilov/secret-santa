97b347a19c72cd26eceaca5fed6e1079e958c8a8
Artur Samoilov <artsamoilov@gmail.com>
2023-12-02 04:22:03 +0100

    feat: set up backend with database WIP

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  res.json({ message: "success" });
+});
+
+app.listen(8080, () => {
+  console.log("Server running on localhost:8080");
+});
